c7d5782a511d54dec52a923fc42cdf0cffe03ac2
unknown <dimaskupejko@gmail.com>
2021-06-27 13:42:59 +0300

    Direct3D: Getting started: Lesson 2: #1

--- Source.cpp
+++ Source.cpp
@@ -2,6 +2,17 @@
 #include <windows.h>
 #include <windowsx.h>
 
+// Direct3D headers
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <d3dx10.h>
+
+// include the Direct3D Library file
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "d3dx11.lib")
+#pragma comment (lib, "d3dx10.lib")
+
+
 // the WindowProc function prototype
 LRESULT CALLBACK WindowProc(HWND hWnd,
 							UINT message,
