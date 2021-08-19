9d7c154e85069bb8479de96c9dfd4721d3a6c018
unknown <dimaskupejko@gmail.com>
2021-08-19 18:02:30 +0300

    Revising the lessons: Lesson 3: finished

--- Source.cpp
+++ Source.cpp
@@ -1,16 +1,69 @@
 #include <windows.h>
+#include <windowsx.h>
+
+LRESULT CALLBACK WindowProc(HWND hWnd, 
+							UINT message,
+							WPARAM wParam,
+							LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInstance,
 				   HINSTANCE hPrevInstance,
 				   LPSTR lpCmdLine,
-				   int nShowCmd)
+				   int nCmdShow)
 {
-	MessageBox(NULL,
-				L"Hello, world!",
-				L"Just another hello world program!",
-				MB_ICONEXCLAMATION | MB_OK);
+	HWND hWnd;
+	WNDCLASSEX wc;
+
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_WINDOW);
+	wc.lpszClassName = L"WindowClass1";
+
+	RegisterClassEx(&wc);
+
+	hWnd = CreateWindowEx(NULL,
+						  L"WindowClass1",
+						  L"Our first windowed program!",
+						  WS_OVERLAPPEDWINDOW,
+						  300,
+						  300,
+						  500,
+						  400,
+						  NULL,
+						  NULL,
+						  hInstance,
+						  NULL);
+
+	ShowWindow(hWnd, nCmdShow);
 
 
-	// test
-	return 0;
+	MSG msg;
+
+	while (GetMessage(&msg, hWnd, 0, 0))
+	{
+		TranslateMessage(&msg);
+
+		DispatchMessage(&msg);
+	}
+
+	return msg.wParam;
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+		case WM_DESTROY:
+		{
+			PostQuitMessage(0);
+			return 0;
+		}
+	}
+
+	return DefWindowProc(hWnd, message, wParam, lParam);
 }
